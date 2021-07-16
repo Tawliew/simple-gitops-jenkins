ed9310a6c5e4d59f0939219a34e2dad8376fcd81
Luiz Fernando <lfluiz.lf@gmail.com>
2021-07-16 10:15:40 -0300

    test kubernetes jenkins

--- simple-gitops-jenkins.groovy
+++ simple-gitops-jenkins.groovy
@@ -1,6 +1,36 @@
 pipeline
 {
-    agent any
+    agent
+    {
+      kubernetes
+      {
+        yaml: '''
+apiVersion: v1
+kind: Pod
+spec:
+  volumes:
+  - name: docker-socket
+    emptyDir: {}
+  containers:
+  - name: docker
+    image: docker:19.03.1
+    command:
+    - sleep
+    args:
+    - 99d
+    volumeMounts:
+    - name: docker-socket
+      mountPath: /var/run
+  - name: docker-daemon
+    image: docker:19.03.1-dind
+    securityContext:
+      privileged: true
+    volumeMounts:
+    - name: docker-socket
+      mountPath: /var/run
+'''
+      }
+    }
     stages 
     {
         stage ('Clone Repo')
