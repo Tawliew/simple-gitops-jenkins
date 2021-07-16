10d1519826e5bc297f2b39bc1598c9222b354735
Luiz Fernando <lfluiz.lf@gmail.com>
2021-07-16 00:32:03 -0300

    fix yaml

--- simple-gitops-jenkins.groovy
+++ simple-gitops-jenkins.groovy
@@ -4,23 +4,23 @@ pipeline
     {
         kubernetes {
             //cloud 'kubernetes'
-            label 'mypod'
+        label 'mypod'
             yaml """
-        apiVersion: v1
-        kind: Pod
-        spec:
-        containers:
-        - name: docker
-            image: docker:1.11
-            command: ['cat']
-            tty: true
-            volumeMounts:
-            - name: dockersock
-            mountPath: /var/run/docker.sock
-        volumes:
-        - name: dockersock
-            hostPath:
-            path: /var/run/docker.sock
+apiVersion: v1
+kind: Pod
+spec:
+containers:
+- name: docker
+    image: docker:1.11
+    command: ['cat']
+    tty: true
+    volumeMounts:
+    - name: dockersock
+    mountPath: /var/run/docker.sock
+volumes:
+- name: dockersock
+    hostPath:
+    path: /var/run/docker.sock
         """
         }
     }
