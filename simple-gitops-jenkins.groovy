667518c086fa85386bad477baf42bf78ecde9521
Luiz Fernando <lfluiz.lf@gmail.com>
2021-07-16 00:37:53 -0300

    change to agent any again

--- simple-gitops-jenkins.groovy
+++ simple-gitops-jenkins.groovy
@@ -1,27 +1,6 @@
 pipeline
 {
-    agent 
-    {
-        kubernetes {
-            yaml """
-apiVersion: v1
-kind: Pod
-spec:
-containers:
-- name: docker
-    image: docker:1.11
-    command: ['cat']
-    tty: true
-    volumeMounts:
-    - name: dockersock
-    mountPath: /var/run/docker.sock
-volumes:
-- name: dockersock
-    hostPath:
-    path: /var/run/docker.sock
-        """
-        }
-    }
+    agent any
 
     stages 
     {
