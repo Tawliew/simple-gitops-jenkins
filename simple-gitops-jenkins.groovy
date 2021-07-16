c15c94136ba5a198f505d1135c4266033cbf9b83
Luiz Fernando <lfluiz.lf@gmail.com>
2021-07-16 01:06:32 -0300

    final test

--- simple-gitops-jenkins.groovy
+++ simple-gitops-jenkins.groovy
@@ -1,6 +1,41 @@
 pipeline
 {
-    agent any
+    agent 
+    {
+        kubernetes 
+        {
+            yaml """
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    jenkins/kube-default: true
+    app: jenkins
+    component: agent
+spec:
+  containers:
+    - name: jnlp
+      image: jenkins/inbound-agent:4.6-1
+      env:
+      - name: POD_IP
+        valueFrom:
+          fieldRef:
+            fieldPath: status.podIP
+      - name: DOCKER_HOST
+        value: tcp://localhost:2375
+    - name: dind
+      image: docker:18.05-dind
+      securityContext:
+        privileged: true
+      volumeMounts:
+        - name: dind-storage
+          mountPath: /var/lib/docker
+  volumes:
+    - name: dind-storage
+      emptyDir: {}
+"""
+        }
+    }
 
     stages 
     {
